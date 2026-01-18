c027efc1a7841a675a3ba14ba04cfd88009346e9
Robson Henrique Rodrigues <robhenrod@gmail.com>
2026-01-18 12:24:30 -0300

    Componentes do Design System - Componente: Avatar

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -21,9 +21,9 @@ const config: StorybookConfig = {
     getAbsolutePath('@storybook/addon-onboarding'),
   ],
   framework: getAbsolutePath('@storybook/react-vite'),
-  docs: {
-    defaultName: 'Documentation',
-    docsMode: true,
-  },
+  // docs: {
+  //   defaultName: 'Documentation',
+  //   docsMode: true,
+  // },
 };
 export default config;
---
+++ packages/react/src/components/Avatar/styles.ts
@@ -0,0 +1,32 @@
+import * as Avatar from '@radix-ui/react-avatar';
+import styled from 'styled-components';
+
+export const AvatarContainer = styled(Avatar.Root)`
+  border-radius: ${({ theme }) => theme.radii.full};
+  display: inline-block;
+  width: ${({ theme }) => theme.space[12]};
+  height: ${({ theme }) => theme.space[12]};
+  overflow: hidden;
+`;
+
+export const AvatarImage = styled(Avatar.Image)`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: inherit;
+`;
+
+export const AvatarFallback = styled(Avatar.Fallback)`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.colors.gray600};
+  color: ${({ theme }) => theme.colors.gray800};
+
+  svg {
+    width: ${({ theme }) => theme.space[6]};
+    height: ${({ theme }) => theme.space[6]};
+  }
+`;
