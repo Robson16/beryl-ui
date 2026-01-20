3e2b7523700da934434d9eef700c0edef834da37
Robson Henrique Rodrigues <robhenrod@gmail.com>
2026-01-20 13:19:07 -0300

    Componentes do Design System - Componente: Checkbox

---
+++ packages/docs/src/stories/Checkbox.stories.tsx
@@ -0,0 +1,28 @@
+import { Box, Checkbox, CheckboxProps, Text } from '@ignite-ui/react';
+import type { Meta, StoryObj } from '@storybook/react';
+import { styled } from 'styled-components';
+
+const StoryBox = styled(Box)`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: ${({ theme }) => theme.space[2]};
+`;
+
+export default {
+  title: 'Form/Checkbox',
+  component: Checkbox,
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <StoryBox as="label">
+          {Story()}
+          <Text size="sm">Accept terms of use</Text>
+        </StoryBox>
+      );
+    },
+  ],
+} as Meta<CheckboxProps>;
+
+export const Primary: StoryObj<CheckboxProps> = {};
---
+++ packages/react/src/components/Checkbox/styles.ts
@@ -0,0 +1,51 @@
+import * as Checkbox from '@radix-ui/react-checkbox';
+import { keyframes, styled } from 'styled-components';
+
+export const CheckboxContainer = styled(Checkbox.Root)`
+  all: unset;
+  width: ${({ theme }) => theme.space[6]};
+  height: ${({ theme }) => theme.space[6]};
+  background-color: ${({ theme }) => theme.colors.gray900};
+  border-radius: ${({ theme }) => theme.radii.xs};
+  line-height: 0;
+  cursor: pointer;
+  overflow: hidden;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 2px solid ${({ theme }) => theme.colors.gray900};
+
+  &:focus,
+  &[data-state='checked'] {
+    border: 2px solid ${({ theme }) => theme.colors.ignite300};
+  }
+
+  &[data-state='checked'] {
+    background-color: ${({ theme }) => theme.colors.ignite300};
+  }
+`;
+
+const slideIn = keyframes`
+  from { transform: translateY(-100%); }
+  to { transform: translateY(0); }
+`;
+
+const slideOut = keyframes`
+  from { transform: translateY(0); }
+  to { transform: translateY(-100%); }
+`;
+
+export const CheckboxIndicator = styled(Checkbox.Indicator)`
+  color: ${({ theme }) => theme.colors.white};
+  width: ${({ theme }) => theme.space[4]};
+  height: ${({ theme }) => theme.space[4]};
+
+  &[data-state='checked'] {
+    animation: ${slideIn} 200ms ease-out;
+  }
+
+  &[data-state='unchecked'] {
+    animation: ${slideOut} 200ms ease-out;
+  }
+`;
